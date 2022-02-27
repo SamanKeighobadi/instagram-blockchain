8a85206a5e53db2b5c8ea2a43821da781e63be22
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-27 20:38:40 +0330

    customize configuration

--- truffle-config.js
+++ truffle-config.js
@@ -3,10 +3,12 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client/src/abi"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      port: 7545,
+      host:"127.0.0.1",
+      network_id:"*",
     }
   }
 };
