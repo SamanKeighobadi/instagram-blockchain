cb35d7a1a6d7fa4fd4a7018ac9b068d76d003fbd
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-02-28 13:14:39 +0330

    feat:  validation uploadImage agruments
    make sure image hash description and address exists

--- contracts/Instagram.sol
+++ contracts/Instagram.sol
@@ -1,31 +1,41 @@
 pragma solidity ^0.5.0;
 
 contract Instagram {
-    string public name ='Instagram Clone on blockchain';
-    uint public postCount = 0;
-    struct Image{
-        uint id;
+    string public name = "Instagram Clone on blockchain";
+    uint256 public postCount = 0;
+    struct Image {
+        uint256 id;
         string hash; // location of IPFS
         string description;
-        uint tipAmount;
+        uint256 tipAmount;
         address payable auther;
     }
 
-    mapping(uint => Image) public images;
+    mapping(uint256 => Image) public images;
 
     event uploadedImage(
-         uint id,
+        uint256 id,
         string hash,
         string description,
-        uint tipAmount,
+        uint256 tipAmount,
         address payable auther
     );
 
-    function uploadImage(string memory _hash,string memory _description,
- ) public{
-        postCount ++;
-        images[postCount]= Image(postCount,_hash,_description,0,msg.sender);
-        emit uploadedImage(postCount,_hash,_description,0,mgs.sender);
-    }
+    function uploadImage(string memory _hash, string memory _description)
+        public
+    {
+        require(bytes(_hash).length > 0);
+        require(bytes(_description).length > 0);
+        require(msg.sender != address(0x0));
 
+        postCount++;
+        images[postCount] = Image(
+            postCount,
+            _hash,
+            _description,
+            0,
+            msg.sender
+        );
+        emit uploadedImage(postCount, _hash, _description, 0, msg.sender);
+    }
 }
