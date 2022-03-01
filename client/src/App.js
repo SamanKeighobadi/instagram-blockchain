7a5f0ebb470bac7df433490904efd6c569f9d93b
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-01 21:15:31 +0330

    connect smart contract to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React,{useState,useEffect} from "react";
 import "./App.css";
 import Web3 from 'web3';
 import Jdenticon from 'react-jdenticon';
+import Ethtagram from './abis/Instagram.json'
 
 const App = () =>{
 
   const [account,setAccount] = useState('')
   
+  const contractAddress = '0xc8a86fe1de9EF9A6895755bb2867F912dF906978' 
 
   const loadWeb3 = async () =>{
     if(window.ethereum){
@@ -23,7 +25,13 @@ const App = () =>{
     const web3 = window.web3;
     const account= await web3.eth.getAccounts();
     setAccount(account[0])
-    
+    const networkId = await web3.eth.net.getId();
+    const networkData = Ethtagram.networks[networkId]
+    if(networkData){
+      const ethtagram = new web3.eth.Contract(Ethtagram.abi,networkData.address)
+    }else {
+      alert('contract not deployed')
+    }
   }
 
   useEffect(() =>{
